1c4ed67130c7e9b6ce1452f684cf1fa8ddd952c1
Jongyoon Ko <hobi0708@gmail.com>
2025-04-11 15:14:39 -0400

    Pistol
    black, silver
    audio

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float bulletSpeed = 20f;
+    [SerializeField] private AudioClip gunshotSound;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void FireBullet()
+    {
+        GameObject spawnBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+        spawnBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * bulletSpeed;
+        Destroy(spawnBullet, 5f);
+
+        if (gunshotSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(gunshotSound);
+        }
+    }
+}
